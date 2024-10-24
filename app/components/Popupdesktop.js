89b535733f1e59ce1bff0345b8ac716069e13833
Burak Durmuşoğlu <108025275+burak2482@users.noreply.github.com>
2024-10-24 19:49:41 +0300

    Update Popupdesktop.js

--- app/components/Popupdesktop.js
+++ app/components/Popupdesktop.js
@@ -1,13 +1,14 @@
-import "./globals.css";
-import Image from "next/image";
+import './globals.css';
+import Image from 'next/image';
 
 export default function Popupdesktop() {
   return (
-    <div className=" bg-customGrey p-20 flex flex-row items-center w-full h-24">
-      <Image src="/icon-share.svg" alt="Share" width={24} height={24} className="hover:text-gray-400 cursor-pointer" />
-      <Image src="/icon-facebook.svg" alt="Facebook" width={24} height={24} className="hover:text-gray-400 cursor-pointer" />
-      <Image src="/icon-twitter.svg" alt="Twitter" width={24} height={24} className="hover:text-gray-400 cursor-pointer" />
-      <Image src="/icon-pinterest.svg" alt="Pinterest" width={24} height={24} className="hover:text-gray-400 cursor-pointer" />
+    <div className=' bg-customGrey p-20 flex flex-row items-center w-full h-24'>
+      <Image src='/icon-share.svg' alt='Share' width={24} height={24} className='hover:text-gray-400 cursor-pointer' />
+      <Image src='/icon-facebook.svg' alt='Facebook' width={24} height={24} className='hover:text-gray-400 cursor-pointer' />
+      <Image src='/icon-twitter.svg' alt='Twitter' width={24} height={24} className='hover:text-gray-400 cursor-pointer' />
+      <Image src='/icon-pinterest.svg' alt='Pinterest' width={24} height={24} className='hover:text-gray-400 cursor-pointer' />
     </div>
   );
 }
+
